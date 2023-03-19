917c66bc683a74bc6c2d116b1480e15104ab0687
Tillerpiggo <36749446+Tillerpiggo@users.noreply.github.com>
2023-03-18 23:22:30 -0700

    Initial Commit

---
+++ JTutorial/JTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  JTutorialApp.swift
+//  JTutorial
+//
+//  Created by Tyler Gee on 3/18/23.
+//
+
+import SwiftUI
+
+@main
+struct JTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
